e489402a0ec0974d00e72158700536c0a131877d
NikolasMosca <ciokkkolate@yahoo.it>
2014-09-10 13:38:55 +0200

    Update R.java

--- src/android/com/nikolas/gallery/R.java
+++ src/android/com/nikolas/gallery/R.java
@@ -35,7 +35,7 @@ public final class R {
         public static final int view_pager=0x7f060002;
     }
     public static final class layout {
-        public static final int activity_view_pager=0x7f030000;
+        public static final int activity_view_pager=0x7f030001;
     }
     public static final class menu {
         public static final int main_menu=0x7f050000;
